a9f0be40bc21699ae3babe8dc7240fdba97e9155
minhhnn <minhhnn@vng.com.vn>
2020-09-30 16:18:46 +0700

    change logic show error

--- app/src/main/java/com/nhatminh/example/architecture/demoarchitecture/search/view/MainActivity.kt
+++ app/src/main/java/com/nhatminh/example/architecture/demoarchitecture/search/view/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.nhatminh.example.architecture.demoarchitecture.search.view
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LifecycleOwner
@@ -16,6 +17,7 @@ import com.nhatminh.example.architecture.demoarchitecture.repository.GithubApi
 import com.nhatminh.example.architecture.demoarchitecture.repository.RetrofitClient
 import com.nhatminh.example.architecture.demoarchitecture.search.viewmodel.SearchViewModel
 import com.nhatminh.example.architecture.demoarchitecture.search.viewmodel.SearchViewModelFactory
+import com.nhatminh.example.architecture.demoarchitecture.model.STATE
 
 
 class MainActivity : AppCompatActivity(), LifecycleOwner{
@@ -59,7 +61,21 @@ class MainActivity : AppCompatActivity(), LifecycleOwner{
         val reposListObserver : Observer<ReposData> = Observer {
             reposData -> adapter.updateResults(reposData.reposList)
             adapter.notifyDataSetChanged()
+            handleError(reposData.state)
         }
         viewModel.reposData.observe(this, reposListObserver)
+    }
+
+    private fun handleError(state : STATE){
+        when(state){
+            STATE.ERROR_NETWORK -> showToast("Network error")
+            STATE.ERROR_INVALID_QUERY -> binding.etSearchQuery.error = "Invalid query"
+            STATE.ERROR_FAILED_RESPONSE -> showToast("Failed response")
+            STATE.ERROR_UNKNOWN -> showToast("Something went wrong")
+        }
+    }
+
+    private fun showToast(message : String){
+        Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
     }
 }
