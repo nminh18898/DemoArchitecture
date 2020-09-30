639beba24b41337150440ed5010988f4d5e2988c
minhhnn <minhhnn@vng.com.vn>
2020-09-30 15:47:15 +0700

    add repos count

--- app/src/main/java/com/nhatminh/example/architecture/demoarchitecture/search/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/nhatminh/example/architecture/demoarchitecture/search/viewmodel/SearchViewModel.kt
@@ -1,24 +1,14 @@
 package com.nhatminh.example.architecture.demoarchitecture.search.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.nhatminh.example.architecture.demoarchitecture.model.ReposData
 import com.nhatminh.example.architecture.demoarchitecture.repository.DataRepository
 
 class SearchViewModel (private val repository: DataRepository) : ViewModel() {
-
-    /*private var _searchRepos : MutableLiveData<MutableList<GithubRepos>> = MutableLiveData(ArrayList())
-    val searchRepos : LiveData<List<GithubRepos>>
-        get() = _searchRepos as LiveData<List<GithubRepos>>
-
-    private var _error : MutableLiveData<ERROR> = MutableLiveData(ERROR.NONE)
-    val error : LiveData<ERROR>
-        get() = _error*/
-
-    /*private var _reposData : MutableLiveData<ReposData> = MutableLiveData(ReposData())
-    val reposData : LiveData<ReposData>
-        get() = _reposData*/
 
     var reposData : LiveData<ReposData> = MutableLiveData((ReposData()))
 
