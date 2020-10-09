defef26c1673abf8a305e7ef2d4d61c4e1223087
minhhnn <minhhnn@vng.com.vn>
2020-10-09 14:08:01 +0700

    add coroutine dependency

--- app/src/main/java/com/nhatminh/example/architecture/demoarchitecture/search/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/nhatminh/example/architecture/demoarchitecture/search/viewmodel/SearchViewModel.kt
@@ -1,6 +1,5 @@
 package com.nhatminh.example.architecture.demoarchitecture.search.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
