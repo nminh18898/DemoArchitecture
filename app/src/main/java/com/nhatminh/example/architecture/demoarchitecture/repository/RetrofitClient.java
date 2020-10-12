82ec656b94927581c430311ebda54ec6aedc30eb
minhhnn <minhhnn@vng.com.vn>
2020-10-12 16:11:45 +0700

    fix singleton multithreaded

--- app/src/main/java/com/nhatminh/example/architecture/demoarchitecture/repository/RetrofitClient.java
+++ app/src/main/java/com/nhatminh/example/architecture/demoarchitecture/repository/RetrofitClient.java
@@ -5,17 +5,21 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    private static Retrofit retrofit = null;
+    private static volatile Retrofit retrofit = null;
 
     private static final String GITHUB_BASE_URL = "https://api.github.com";
 
 
     public static Retrofit getClient() {
         if (retrofit==null) {
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(GITHUB_BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
+            synchronized (RetrofitClient.class) {
+                if(retrofit == null) {
+                    retrofit = new Retrofit.Builder()
+                            .baseUrl(GITHUB_BASE_URL)
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .build();
+                }
+            }
         }
         return retrofit;
     }
